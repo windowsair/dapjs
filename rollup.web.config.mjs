5a572b95b4350cf0a07195507c136e5932fd0e82
windowsair <msdn_02@sina.com>
2024-06-04 22:52:14 +0800

    Update rollup to v4.18.0
    The initial build is much faster than the previous version:
    
    35s -> 5s.

--- rollup.web.config.mjs
+++ rollup.web.config.mjs
@@ -1,27 +1,25 @@
 import del from 'rollup-plugin-delete';
 import eslint from '@rollup/plugin-eslint';
-import builtins from 'rollup-plugin-node-builtins';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
-import sourceMaps from 'rollup-plugin-sourcemaps'
+import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 
 const name = 'DAPjs';
-const pkg = require('./package.json')
 const watch = process.env.ROLLUP_WATCH;
 
 export default {
     input: 'src/index.ts',
     output: [
         {
-            file: pkg.main,
+            file: 'dist/dap.umd.js',
             format: 'umd',
             sourcemap: true,
             name
         },
         {
-            file: pkg.module,
+            file: 'dist/dap.esm.js',
             format: 'esm',
             sourcemap: true
         }
@@ -36,12 +34,11 @@ export default {
         eslint({
             throwOnError: true
         }),
-        builtins(),
+        nodePolyfills(),
         typescript({
             useTsconfigDeclarationDir: true
         }),
         terser(),
-        sourceMaps(),
         watch && serve({
             contentBase: '.',
             open: true,
